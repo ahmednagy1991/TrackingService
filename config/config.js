d2277f8b34d0b9edc3a5136d5b37833560fb7b48
ahmed nagy <ahmed_nagy1991@hotmail.com>
2019-12-13 11:30:56 +0200

    fix

--- config/config.js
+++ config/config.js
@@ -4,7 +4,9 @@
 */
 let Config = function () {
 	return {
-		dbPath: "mongodb://localhost:27017/TrackingAPI", 
+		//mongodb://ahmednagy:<password>@cluster0-shard-00-00-5p48n.mongodb.net:27017,cluster0-shard-00-01-5p48n.mongodb.net:27017,cluster0-shard-00-02-5p48n.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
+		//dbPath: "mongodb+srv://ahmednagy:Ahmed1991@cluster0-5p48n.mongodb.net/test?retryWrites=true&w=majority/Tracking", 
+		dbPath: process.env.CONN
 	};
 };
 
